64c3acf5ec74d893c3789a7f12bda41ec375a29b
FedericoViga <202783519+FedericoViga@users.noreply.github.com>
2025-07-19 17:47:56 +0200

    ripristinati tutti i valori dinamici in hero HeroRecap e pagina giochi

--- app/_components/HeroRecap.jsx
+++ app/_components/HeroRecap.jsx
@@ -9,7 +9,7 @@ async function GameCount({ numGames }) {
   return (
     <Link href="/games">
       <div className="flex items-center justify-center gap-1">
-        <span className="text-5xl font-bold text-blue-500">421</span>
+        <span className="text-5xl font-bold text-blue-500">{numGames}</span>
         <p className="text-primary text-3xl font-bold underline decoration-2 underline-offset-3">
           {numGames !== 1 ? "GIOCHI" : "GIOCO"}
         </p>
@@ -22,9 +22,11 @@ async function CollectorCount({ numCollectors }) {
   return (
     <Link href="/games/collectors">
       <div className="flex items-center justify-center gap-1">
-        <span className="text-5xl font-bold text-blue-500">26</span>
+        <span className="text-5xl font-bold text-blue-500">
+          {numCollectors}
+        </span>
         <p
-          className={`text-primary font-bold underline decoration-2 underline-offset-3 ${10 > 9 ? "text-2xl" : "text-3xl"}`}
+          className={`text-primary font-bold underline decoration-2 underline-offset-3 ${numCollectors > 9 ? "text-2xl" : "text-3xl"}`}
         >
           {numCollectors !== 1 ? "COLLECTOR'S EDITIONS" : "COLLECTOR'S EDITION"}
         </p>
