e1defcc465441c413c86df6067f26839fb006b91
FedericoViga <202783519+FedericoViga@users.noreply.github.com>
2025-07-20 12:50:34 +0200

    cambiato stile PlatformSelectorButton

--- app/_components/PlatformSelectorButton.jsx
+++ app/_components/PlatformSelectorButton.jsx
@@ -6,7 +6,7 @@ function PlatformSelectorButton({ onOpenFilters, filter, isExpanded }) {
   return (
     <button
       onClick={() => onOpenFilters((isExp) => !isExp)}
-      className={`text-primary cursor-pointer rounded-lg border-2 border-slate-600 p-1 text-sm ${isExpanded || (filter !== "Tutte" && filter !== undefined) ? "!text-foreground border-2 !border-blue-500" : "border-primary border"}`}
+      className={`text-primary cursor-pointer rounded-lg border-2 border-slate-600 px-2 py-1 text-sm ${isExpanded || (filter !== "Tutte" && filter !== undefined) ? "!text-foreground border-2 !border-blue-500" : "border-primary border"}`}
     >
       {searchParams.get("platform") === "all" || !searchParams.has("platform")
         ? "Tutte"
