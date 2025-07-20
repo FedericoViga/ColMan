18144cdf0c915b1049bb7d4e8509d3d6758f19ed
FedericoViga <202783519+FedericoViga@users.noreply.github.com>
2025-07-20 15:48:20 +0200

    aggiunti link e info alla pagina di login

--- app/login/page.js
+++ app/login/page.js
@@ -3,10 +3,40 @@ import SigninButton from "../_components/SigninButton";
 function Page() {
   return (
     <div className="section flex min-h-dvh flex-col items-center justify-center gap-10 text-center">
-      <h2 className="text-primary text-lg font-bold">
-        Accedi per visualizzare ColMan
-      </h2>
-      <SigninButton />
+      <h1 className="text-primary text-5xl font-bold">ColMan</h1>
+      <div className="gap- flex flex-col">
+        <p>
+          ColMan sta per Collection Manager ed è una Single Page Application
+          sviluppata da{" "}
+          <a
+            href="https://portfolio-federico-vigano.vercel.app/"
+            className="text-blue-400 underline underline-offset-3"
+          >
+            Federico Viganò
+          </a>
+          &nbsp;per uso personale.
+        </p>
+
+        <p>
+          Maggiori informazioni su{" "}
+          <a
+            href="https://github.com/FedericoViga"
+            className="text-blue-400 underline underline-offset-3"
+          >
+            GitHub
+          </a>
+        </p>
+      </div>
+
+      <div className="flex flex-col items-center gap-4">
+        <h2 className="text-primary text-lg font-bold">
+          Accedi per visualizzare ColMan
+        </h2>
+        <p>Acceso autorizzato solo per uno specifico account Google</p>
+        <SigninButton />
+      </div>
+
+      <span className="text-primary fixed bottom-2">2025 Federico Viganò</span>
     </div>
   );
 }
