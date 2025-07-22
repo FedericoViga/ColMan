9d1336d0ce3b518e587840a3dba6512d9821f347
FedericoViga <202783519+FedericoViga@users.noreply.github.com>
2025-07-22 16:24:02 +0200

    aggiunto loader per feedback visivo durante server action di eliminazione gioco e piattaforma

--- app/_components/DeleteGameButton.jsx
+++ app/_components/DeleteGameButton.jsx
@@ -2,24 +2,42 @@
 
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { deleteGame } from "../_lib/actions";
+import { useTransition } from "react";
+import DeleteLoader from "./DeleteLoader";
 
 function DeleteGame({ gameId, gameName, gameImages }) {
+  const [isPending, startTransition] = useTransition();
+
   function handleDelete() {
     const confirmation = window.confirm(
       `Sei sicuro di voler cancellare ${gameName.toUpperCase()}?`,
     );
     if (confirmation) {
-      deleteGame(gameId, gameImages);
+      startTransition(deleteGame(gameId, gameImages));
     }
   }
 
   return (
-    <div className="mt-5 flex min-w-24 flex-3/6 cursor-pointer items-center justify-center gap-1 self-start rounded border-2 border-red-500 py-1 text-base">
-      <TrashIcon className="h-4 w-4" />
-      <button className="cursor-pointer" onClick={handleDelete}>
-        Elimina
-      </button>
-    </div>
+    <>
+      {!isPending ? (
+        <div className="mt-5 flex min-w-24 flex-3/6 cursor-pointer items-center justify-center gap-1 self-start rounded border-2 border-red-500 py-1 text-base">
+          <TrashIcon className="h-4 w-4" />
+          <button className="cursor-pointer" onClick={handleDelete}>
+            Elimina
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className="mt-5 flex min-w-24 flex-3/6 cursor-pointer items-center justify-center gap-1 self-start rounded border-2 border-red-500 py-1 text-base">
+            <TrashIcon className="h-4 w-4" />
+            <button className="cursor-pointer" onClick={handleDelete}>
+              Elimina
+            </button>
+          </div>
+          <DeleteLoader />
+        </>
+      )}
+    </>
   );
 }
 
---
+++ app/_components/DeleteLoader.jsx
@@ -0,0 +1,11 @@
+function DeleteLoader() {
+  return (
+    <div className="bg-background fixed top-0 left-0 z-50 h-full w-full opacity-90">
+      <div className="mx-auto mb-7 flex h-full items-center justify-center gap-1">
+        <div className="delete-loader">Eliminazione</div>
+      </div>
+    </div>
+  );
+}
+
+export default DeleteLoader;
