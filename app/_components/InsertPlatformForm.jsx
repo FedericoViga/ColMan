c5f4d5306fb820d2a59f9d102f7bdb7e590cea86
FedericoViga <202783519+FedericoViga@users.noreply.github.com>
2025-07-22 11:40:04 +0200

    aggiunti indicatori di lunghezza input in crea/modifica piattforma

--- app/_components/InsertPlatformForm.jsx
+++ app/_components/InsertPlatformForm.jsx
@@ -2,15 +2,23 @@
 
 import { useFormStatus } from "react-dom";
 import { insertPlatform } from "../_lib/actions";
+import { useState } from "react";
 
 function InsertPlatformForm() {
+  const [nameLength, setNameLength] = useState(0);
+  const [ownerLength, setOwnerLength] = useState(0);
+
   return (
     <div className="container">
       <form action={insertPlatform}>
         <div className="my-5 flex flex-col justify-items-start gap-4 py-5 text-lg">
           <h1 className="text-center text-2xl">Crea una nuova piattaforma</h1>
+
           <div className="flex flex-col gap-1">
-            <label className="text-primary">Nome</label>
+            <div className="flex items-baseline justify-between">
+              <label className="text-primary">Nome</label>
+              <span className="text-primary text-sm">{nameLength}/25</span>
+            </div>
             <input
               autoCapitalize="sentences"
               required
@@ -18,12 +26,16 @@ function InsertPlatformForm() {
               type="text"
               className="border-primary rounded border p-1.5 text-base"
               autoComplete="off"
-              maxLength="50"
+              maxLength="25"
+              onChange={(e) => setNameLength(e.target.value.length)}
             />
           </div>
 
           <div className="flex flex-col gap-1">
-            <label className="text-primary">Produttore</label>
+            <div className="flex items-baseline justify-between">
+              <label className="text-primary">Produttore</label>
+              <span className="text-primary text-sm">{ownerLength}/25</span>
+            </div>
             <input
               autoCapitalize="sentences"
               required
@@ -31,7 +43,8 @@ function InsertPlatformForm() {
               type="text"
               className="border-primary rounded border p-1.5 text-base"
               autoComplete="off"
-              maxLength="50"
+              maxLength="25"
+              onChange={(e) => setOwnerLength(e.target.value.length)}
             />
           </div>
 
--- app/_components/UpdatePlatformForm.jsx
+++ app/_components/UpdatePlatformForm.jsx
@@ -5,9 +5,12 @@ import { useFormStatus } from "react-dom";
 import { ArrowPathIcon } from "@heroicons/react/24/solid";
 import { updatePlatform } from "../_lib/actions";
 import toast from "react-hot-toast";
+import { useState } from "react";
 
 function UpdatePlatformForm({ platformDetails }) {
   const { id, platformName, platformOwner } = platformDetails;
+  const [nameLength, setNameLength] = useState(platformName.length);
+  const [ownerLength, setOwnerLength] = useState(platformOwner.length);
 
   async function handleUpdatePlatform(formData) {
     const res = await updatePlatform(formData);
@@ -26,26 +29,36 @@ function UpdatePlatformForm({ platformDetails }) {
 
         <div className="mt-3 mb-4 flex flex-col justify-items-start gap-4 py-5 text-lg">
           <div className="flex flex-col gap-1">
-            <label className="text-primary">Piattaforma</label>
+            <div className="flex items-baseline justify-between">
+              <label className="text-primary">Piattaforma</label>
+              <span className="text-primary text-sm">{nameLength}/25</span>
+            </div>
+
             <input
               required
               name="platformName"
               type="text"
               className="border-primary rounded border p-1.5 text-base"
               defaultValue={platformName}
-              maxLength="50"
+              maxLength="25"
+              onChange={(e) => setNameLength(e.target.value.length)}
             />
           </div>
 
           <div className="flex flex-col gap-1">
-            <label className="text-primary">Produttore</label>
+            <div className="flex items-baseline justify-between">
+              <label className="text-primary">Produttore</label>
+              <span className="text-primary text-sm">{ownerLength}/25</span>
+            </div>
+
             <input
               required
               name="platformOwner"
               type="text"
               className="border-primary rounded border p-1.5 text-base"
               defaultValue={platformOwner}
-              maxLength="50"
+              maxLength="25"
+              onChange={(e) => setOwnerLength(e.target.value.length)}
             />
           </div>
 
