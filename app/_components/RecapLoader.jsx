0b8d46f713c7f3f6479eb1f92e0d7db819c24c81
FedericoViga <202783519+FedericoViga@users.noreply.github.com>
2025-07-26 12:51:20 +0200

    aggiunto effetto shimmering allo skeleton loader e cambiato il bg color di tutta l'app conun nero puro

--- app/_components/RecapLoader.jsx
+++ app/_components/RecapLoader.jsx
@@ -1,19 +1,19 @@
 function RecapLoader() {
   return (
-    <div className="container flex animate-pulse flex-col items-center justify-center gap-3 py-5">
-      <div className="flex w-64 items-center justify-center gap-3">
-        <div className="min-h-12 min-w-20 rounded bg-blue-950"></div>
-        <div className="min-h-10 min-w-48 rounded bg-gray-500"></div>
+    <div className="container flex flex-col items-center justify-center gap-3 overflow-hidden py-5">
+      <div className="flex w-full items-center justify-center gap-2">
+        <div className="skeleton h-12 w-16 rounded bg-blue-950"></div>
+        <div className="skeleton h-9 w-28 rounded bg-gray-700"></div>
       </div>
 
-      <div className="flex w-64 items-center justify-center gap-3">
-        <div className="min-h-12 min-w-20 rounded bg-blue-950"></div>
-        <div className="min-h-10 min-w-56 rounded bg-gray-500"></div>
+      <div className="flex w-full items-center justify-center gap-2">
+        <div className="skeleton h-12 w-16 rounded-md bg-blue-950"></div>
+        <div className="skeleton h-9 w-64 rounded-md bg-gray-700"></div>
       </div>
 
-      <div className="flex w-64 items-center justify-center gap-3">
-        <div className="min-h-12 min-w-20 rounded bg-blue-950"></div>
-        <div className="min-h-10 min-w-48 rounded bg-gray-500"></div>
+      <div className="flex w-full items-center justify-center gap-2">
+        <div className="skeleton h-12 w-16 rounded-md bg-blue-950"></div>
+        <div className="skeleton h-9 w-40 rounded-md bg-gray-700"></div>
       </div>
     </div>
   );
