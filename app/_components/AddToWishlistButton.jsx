92899a2661aaec00658f4930150f73dce67a3a3e
FedericoViga <202783519+FedericoViga@users.noreply.github.com>
2026-01-18 12:28:17 +0100

    miglioremnti css button add to wishlist

--- app/_components/AddToWishlistButton.jsx
+++ app/_components/AddToWishlistButton.jsx
@@ -11,11 +11,11 @@ function AddToWishlistButton({ onOpenClose }) {
   return (
     <button
       type="button"
-      aria-label="Aggiungi un prodotto alla wishlist"
+      aria-label="Aggiungi un gioco alla wishlist"
       aria-haspopup="dialog"
       aria-controls="wishlist-modal"
       onClick={(e) => handleOpenModal(e)}
-      className="fixed right-6 bottom-8 flex cursor-pointer items-center justify-center bg-transparent text-5xl"
+      className="fixed right-0 bottom-8 flex cursor-pointer items-center justify-center bg-transparent pr-4 text-5xl"
     >
       <HeartPlusIcon className="h-12 w-12 text-blue-500" />
     </button>
